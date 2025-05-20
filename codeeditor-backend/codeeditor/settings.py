7cadf9d83ac3bc97ef30503bc6a95f60ae64baf9
prudhvi1519 <prudhviakula92@gmail.com>
2025-05-20 14:16:01 +0530

    New up

--- codeeditor-backend/codeeditor/settings.py
+++ codeeditor-backend/codeeditor/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='insecure-dev-key')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://python-code-editor-full-stack.onrender.com/']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
